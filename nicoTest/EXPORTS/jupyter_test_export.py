9e7b53ad0b2aa99c4552b17a62d8890081df6e86
nicovernio <nicovernio@gmail.com>
2021-03-27 09:45:03 +1300

    testing exports

--- nicoTest/EXPORTS/jupyter_test_export.py
+++ nicoTest/EXPORTS/jupyter_test_export.py
@@ -2,23 +2,25 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 msg= "helloooo!"
-msg+=" world"
 print( msg)
+msg+=" world"
 
 
 # %%
 a= 3
 b= 2
-while a< 100:
+while a< 1000:
     a, b= b, a+ b
-print( a)
+m= msg+" "+ str( a)
+print( m)
 
 
 # %%
 from numpy import pi, cos, sin, sqrt, arange
 import matplotlib.pyplot as pp
 
-numPts= 1000
+numPts= a #1000
+print( "a: "+ str(a))
 inds= arange( 0, numPts, dtype= float)+ 0.5
 
 r= sqrt( inds/ numPts)
